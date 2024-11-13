a0d087a27873d33a8f24477b1f2a09a4cf952933
vito1012buss <thebestvitodiaries@gmail.com>
2024-11-13 14:20:16 +0700

    tambahin lagi tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 import datetime
+import json
 from django.shortcuts import render, redirect, reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -131,3 +132,21 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
